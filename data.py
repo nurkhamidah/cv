b6e522f92932dea6ce3055fdd7e8fe7a450c48b5
nurkhamidah <nurkhamidah.stat@gmail.com>
2024-06-29 14:42:08 +0700

    finish data cleaning

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
 import pandas as pd
+
+# --------------------------- COLORS
+red = '#ff4c50'
+pink = '#fcacac'
+light_grey = '#f4f3f3'
+green = '#16847c'
+dark_grey = '#4e545b'
+light_green = '#7fd4cd'
+black = '#272829'
+dark_black = '#222224'
--- stream.py
+++ stream.py
@@ -1,1 +1,37 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+from data import *
+
+st.set_page_config(
+    page_title="Nur Khamidah",
+    page_icon="💖",
+    layout='wide',
+)
+
+with open('style.css') as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+st.markdown('<h1>NUR <b>KHAMIDAH</b></h1>', unsafe_allow_html=True)
+
+pages = option_menu("", ['About', 'Experiences', 'Skills & Portfolios', 'Certificates', 'Contact Me'], 
+    icons=['file-person', 'send-arrow-up', 'bag-fill', 'file-earmark-text', 'chat-left-quote'], default_index=0, orientation="horizontal")
+
+# --------------------------- About
+if pages == 'About':
+    pages
+
+# --------------------------- Experiences
+if pages == 'Experiences':
+    pages
+    
+# --------------------------- Skills & Portfolios
+if pages == 'Skills & Portfolios':
+    pages
+    
+# --------------------------- Certificates
+if pages == 'Certificates':
+    pages
+    
+# --------------------------- Contact Me
+if pages == 'Contact Me':
+    pages
