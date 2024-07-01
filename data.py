c7091a029f60f8585dc245c4d309201ff7612945
nurkhamidah <nurkhamidah.stat@gmail.com>
2024-07-01 17:16:47 +0700

    finish for 2nd first tabs

--- data.py
+++ data.py
@@ -1,11 +1,89 @@
 import pandas as pd
+import plotly.figure_factory as ff
+import plotly.express as px
+import re
 
 # --------------------------- COLORS
 red = '#ff4c50'
-pink = '#fcacac'
-light_grey = '#f4f3f3'
-green = '#16847c'
-dark_grey = '#4e545b'
-light_green = '#7fd4cd'
 black = '#272829'
-dark_black = '#222224'
+brown1 = '#985646'
+brown2 = '#bf8859'
+brown3 = '#9c7d61'
+brown4 = '#b9735c'
+green1 = '#4f746c'
+green2 = '#79a99d'
+green3 = '#6d7961'
+blue1 = '#4b7784'
+blue2 = '#6ca2af'
+blue3 = '#a9bcc2'
+nude1 = '#9be7e78'
+nude2 = '#c3b4af'
+nude3 = '#ada08d'
+
+# --------------------------- DATA
+experience = pd.read_excel('Tableau CV-2.xlsx', sheet_name='EXPERIENCE')
+skill = pd.read_excel('Tableau CV-2.xlsx', sheet_name='SKILL')
+language = pd.read_excel('Tableau CV-2.xlsx', sheet_name='LANGUAGES')
+certification = pd.read_excel('Tableau CV-2.xlsx', sheet_name='CERTIFICATION')
+portfolio = pd.read_excel('Tableau CV-2.xlsx', sheet_name='PORTFOLIO')
+
+# --------------------------- FUNCTIONS
+def stringSplit(strs):
+    return(re.split(",", strs))
+
+def diff_month(d1, d2):
+    return (d1.year - d2.year) * 12 + d1.month - d2.month
+
+# --------------------------- TABLE
+experience['Association'] = experience['Association'].apply(stringSplit)
+experience['Tools'] = experience['Tools'].apply(stringSplit)
+experience['Hard Skill'] = experience['Hard Skill'].apply(stringSplit)
+experience['Soft Skill'] = experience['Soft Skill'].apply(stringSplit)
+skill['Associated with'] = skill['Associated with'].apply(stringSplit)
+portfolio['Skill'] = portfolio['Skill'].apply(stringSplit)
+portfolio['Method'] = portfolio['Method'].apply(stringSplit)
+portfolio['Tools'] = portfolio['Tools'].apply(stringSplit)
+
+experience['Duration (Month)'] = experience.apply(lambda x: diff_month(x['End'], x['Start']),
+                                                  axis=1)
+
+# --------------------------- UTILITIES
+
+# --------------------------- About
+## ------------------------------------- TXT
+about = '<div class="txt"><b>Data Scientist</b> with a strong foundation in statistical modeling, machine learning, and data visualization. Proven expertise in <b>Python, R, SQL, and NoSQL databases</b>, with a track record of developing innovative solutions and insightful analytics. Adept at handling large datasets, conducting comprehensive research, and delivering actionable insights. Seeking to leverage my skills and experience to contribute to cutting-edge projects in data science and analytics. Some of the analytical techniques mastered include statistical modeling, analysis of unstructured data such as text data, machine learning including clustering and classification and their anomaly handling and interpretation, and deep learning with BERT.</div>'
+
+# --------------------------- Experience
+## ------------------------------------- GANTT CHART
+df_exp = []
+for i in range(len(experience)):
+    df_exp.append(dict(
+        Task = experience.loc[i, 'Position'],
+        Start = str(experience.loc[i, 'Start']),
+        Finish = str(experience.loc[i, 'End']),
+        Resource = experience.loc[i, 'Type'],
+        Description = experience.loc[i, 'Organization']
+    ))
+
+colors = {'Education': brown1,
+          'Work': brown2,
+          'Course': blue1,
+          'Organization': brown3,
+          'Activity': green3,
+          'Lecture': blue2}
+
+gc = ff.create_gantt(df_exp, colors=colors, index_col='Resource', show_colorbar=True,
+                      group_tasks=True, showgrid_x=True, showgrid_y=True)
+gc.update_layout(autosize=True, title='', title_x=0.5)
+
+## ------------------------------------- BAR CHART
+def makeBarchart(df, col1, col2):
+    df = df.groupby([col1])[col2].sum()
+    fig = px.bar(df, orientation='h')
+    # Customize labels
+    fig.update_xaxes(showgrid=True)
+    fig.update_yaxes(showgrid=True)
+    return(fig)
+
+# --------------------------- CSV
+# --------------------------- CSV
--- stream.py
+++ stream.py
@@ -18,11 +18,86 @@ pages = option_menu("", ['About', 'Experiences', 'Skills & Portfolios', 'Certifi
 
 # --------------------------- About
 if pages == 'About':
-    pages
-
+    ab1, ab2, ab3, ab4, ab5, ab6, ab7 = st.columns([1, 5, 1, 4, 1, 5, 1])
+    with ab2:
+        st.markdown('<h2>ABOUT <b>ME</b></h2>', unsafe_allow_html=True)
+        st.markdown(about, unsafe_allow_html=True)
+        
+    with ab4:
+        " "
+        " "
+        " "
+        st.image('data/img_cv.png')
+    
+    with ab6:
+        " "
+        " "
+        
+        st.markdown('<div class="txt2"><b>Nur Khamidah, S.Stat.</b></div>', unsafe_allow_html=True)
+        st.markdown('<div class="txt2">Bogor Indonesia 16116</div>', unsafe_allow_html=True)
+        " "
+        with st.container():
+            st.markdown('<h2>LINKS</h2>', unsafe_allow_html=True)
+            con1, con1b, con2, con2b, con3, con3b, con4 = st.columns([3,1,3,1,3,1,3])
+            with con1:
+                st.image('data/img_li.png', use_column_width=True)
+                st.page_link(page='https://linkedin.com/in/nurkhamidah', label='LinkedIn')
+            with con2:
+                st.image('data/img_gh.png', use_column_width=True)
+                st.page_link(page='https://github.com/nurkhamidah', label='GitHub')
+            with con3:
+                st.image('data/img_r.png', use_column_width=True)
+                st.page_link(page='https://rpubs.com/nurkhamidah', label='RPubs')
+            with con4:
+                st.image('data/img_rg.png', use_column_width=True)
+                st.page_link(page='https://www.researchgate.net/profile/Nur-Khamidah-3', label='ResearchGate')
+                
+        " "        
+        st.markdown('<div class="txt2">See another version of this CV</div>', unsafe_allow_html=True)
+        col1, col2, col3 = st.columns([1,2,1])
+        with col2:
+            st.page_link(page='https://bit.ly/tableau_nurkhamidah', label='TABLEAU VER', icon='🔗', use_container_width=True)
+            st.download_button(label='⬇️ DOWNLOAD CV', data = 'data/Nur    Khamidah_CV_2.pdf', file_name='CV Nur Khamidah.pdf')
+        
 # --------------------------- Experiences
 if pages == 'Experiences':
-    pages
+    st.markdown('<h2>OVERALL <b>EXPERIENCES</b></h2>', unsafe_allow_html=True)
+    st.plotly_chart(gc)
+    
+    st.markdown('<h2>DURATION OF <b>EXPERIENCES</b></h2>', unsafe_allow_html=True)
+    exp1, exp2 = st.columns([1,5])
+    with exp1:
+        base_exp = st.selectbox('Show Based on:', ['Organization', 'Position'])
+        base_skill = st.checkbox('Filter based on tools/skills')
+        if base_skill:
+            skill_exp = st.selectbox('Choose tools/skills:', ['Tools', 'Hard Skill', 'Soft Skill'])
+            skill_opt = st.multiselect('Choose '+ skill_exp, experience[skill_exp].explode().to_list())
+            experience = experience[[any(x in i for x in skill_opt) for i in experience[skill_exp]]]
+    with exp2:
+        st.plotly_chart(makeBarchart(experience, base_exp, 'Duration (Month)'))
+         
+    st.markdown('<h2>DETAIL <b>EXPERIENCES</b></h2>', unsafe_allow_html=True)
+            
+    exp3, exp4, exp5 = st.columns([1,1,1])
+    with exp3:
+        exp_name = st.selectbox('Choose Position:', experience['Position'].unique())
+    with exp4:
+        exp_org = st.selectbox('Choose Organization:', experience[experience['Position'] == exp_name]['Organization'].unique())
+        
+    exp6, exp7, exp8 = st.columns([1,1,2], gap='medium')
+    dat_exp = experience[experience['Position'] == exp_name][experience['Organization'] == exp_org].reset_index()
+    with exp6:
+        st.markdown('<div class="txt"><b>Experience: </b>{} at {}</div>'.format(dat_exp['Position'][0], dat_exp['Organization'][0]), unsafe_allow_html=True)
+        st.markdown('<div class="txt"><b>Time: </b>{} to {}</div>'.format(dat_exp['Start'].apply(lambda x: x.strftime('%B %Y'))[0], dat_exp['End'].apply(lambda x: x.strftime('%B %Y'))[0]), unsafe_allow_html=True)
+        st.markdown('<div class="txt"><b>Location: </b>{}, {}</div>'.format(dat_exp['City '][0], dat_exp['State'][0]), unsafe_allow_html=True)
+        st.markdown('<div class="txt"><b>Duration: </b>{} years and {} months</div>'.format(dat_exp['Duration (Month)'][0] // 12, dat_exp['Duration (Month)'][0] % 12), unsafe_allow_html=True)
+        st.markdown('<div class="txt"><b>Associated with: </b>{}</div>'.format(', '.join(dat_exp['Association'][0])), unsafe_allow_html=True)
+    with exp7:
+        st.markdown('<div class="txt"><b>Tools used: </b>{}</div>'.format(', '.join(dat_exp['Tools'][0])), unsafe_allow_html=True)
+        st.markdown('<div class="txt"><b>Hard skills used: </b>{}</div>'.format(', '.join(dat_exp['Hard Skill'][0])), unsafe_allow_html=True)
+        st.markdown('<div class="txt"><b>Soft skills used: </b>{}</div>'.format(', '.join(dat_exp['Soft Skill'][0])), unsafe_allow_html=True)
+    with exp8:
+        st.markdown('<div class="txt"><b>Description </b>{}</div>'.format(dat_exp['Description'][0].replace('\n', '<br>')), unsafe_allow_html=True)
     
 # --------------------------- Skills & Portfolios
 if pages == 'Skills & Portfolios':
